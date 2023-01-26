64391edb602295319c67571c028f13749339ca97
dericeric <feigift@gmail.com>
2023-01-27 01:55:14 +0800

    Update configwg-s.sh

--- openwrt/configwg-s.sh
+++ openwrt/configwg-s.sh
@@ -4,5 +4,5 @@ echo >>$ser
 echo "config interface 'wg0'">>$ser
 echo "        option proto 'wireguard'">>$ser
 echo "        option listen_port '23456'">>$ser
-echo "        list addresses '192.168.100.1/24'">>$ser
+echo "        list addresses '10.0.0.1/24'">>$ser
 echo "        option private_key" $serpukey>>$ser
